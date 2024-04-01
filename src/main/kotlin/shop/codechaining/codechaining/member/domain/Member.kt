81a3c62d636996e90db0042f934f1dee470e1ce8
giwoong <dev.choi0409@gmail.com>
2024-04-02 00:47:21 +0900

    refactor: member 변수 수정

--- src/main/kotlin/shop/codechaining/codechaining/member/domain/Member.kt
+++ src/main/kotlin/shop/codechaining/codechaining/member/domain/Member.kt
@@ -5,24 +5,16 @@ import jakarta.persistence.*
 @Entity
 class Member(
     val email: String,
-
     val name: String,
-
     val picture: String,
-
-    nickname: String,
-
+    var nickname: String,
     @Enumerated(EnumType.STRING)
     val role: Role,
-) {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "member_id")
     val memberId: Long? = null
-
-    var nickname: String = nickname
-        private set
-
+) {
     fun updateNickname(nickname: String) {
         this.nickname = nickname
     }
