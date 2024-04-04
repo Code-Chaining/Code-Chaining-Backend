29951d66e5800cf5c822b29b30f794f7a84fadba
giwoong <dev.choi0409@gmail.com>
2024-04-05 01:25:18 +0900

    refactor: 인증된 사용자 email 매개변수 추가

--- src/main/kotlin/shop/codechaining/codechaining/comment/api/CommentController.kt
+++ src/main/kotlin/shop/codechaining/codechaining/comment/api/CommentController.kt
@@ -1,6 +1,7 @@
 package shop.codechaining.codechaining.comment.api
 
 import org.springframework.http.HttpStatus
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 import shop.codechaining.codechaining.comment.api.request.CommentSaveReqDto
 import shop.codechaining.codechaining.comment.api.request.CommentUpdateReqDto
@@ -15,8 +16,8 @@ class CommentController(
 ) {
 
     @PostMapping("/")
-    fun commentSave(@RequestBody commentSaveReqDto: CommentSaveReqDto): RspTemplate<String> {
-        commentService.commentSave("email", commentSaveReqDto)
+    fun commentSave(@AuthenticationPrincipal email: String, @RequestBody commentSaveReqDto: CommentSaveReqDto): RspTemplate<String> {
+        commentService.commentSave(email, commentSaveReqDto)
         return RspTemplate(HttpStatus.CREATED, "댓글 작성")
     }
 
@@ -28,16 +29,17 @@ class CommentController(
 
     @PutMapping("/{commentId}")
     fun commentUpdate(
+        @AuthenticationPrincipal email: String,
         @PathVariable commentId: Long,
         @RequestBody commentUpdateReqDto: CommentUpdateReqDto
     ): RspTemplate<String> {
-        commentService.commentUpdate("email", commentId, commentUpdateReqDto)
+        commentService.commentUpdate(email, commentId, commentUpdateReqDto)
         return RspTemplate(HttpStatus.OK, "댓글 수정")
     }
 
     @DeleteMapping("/{commentId}")
-    fun commentDelete(@PathVariable commentId: Long): RspTemplate<String> {
-        commentService.commentDelete("email", commentId)
+    fun commentDelete(@AuthenticationPrincipal email: String, @PathVariable commentId: Long): RspTemplate<String> {
+        commentService.commentDelete(email, commentId)
         return RspTemplate(HttpStatus.OK, "댓글 삭제")
     }
 
