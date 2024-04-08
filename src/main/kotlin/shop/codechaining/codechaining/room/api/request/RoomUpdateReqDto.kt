eb0640f63ecade63f44586911a4bed3d4b2d8061
giwoong <dev.choi0409@gmail.com>
2024-04-08 16:49:27 +0900

    refactor: 정규식 수정, Dto 예외처리

--- src/main/kotlin/shop/codechaining/codechaining/room/api/request/RoomUpdateReqDto.kt
+++ src/main/kotlin/shop/codechaining/codechaining/room/api/request/RoomUpdateReqDto.kt
@@ -1,13 +1,13 @@
 package shop.codechaining.codechaining.room.api.request
 
-import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
+import shop.codechaining.codechaining.global.annotation.UnicodeNotBlank
 
 data class RoomUpdateReqDto(
-    @field:NotBlank(message = "제목은 비워둘 수 없습니다.")
+    @field:UnicodeNotBlank(message = "제목은 비워둘 수 없습니다.")
     @field:Size(max = 50, message = "제목은 50자를 넘길 수 없습니다.")
     val title: String,
-    @field:NotBlank(message = "내용은 비워둘 수 없습니다.")
+    @field:UnicodeNotBlank(message = "제목은 비워둘 수 없습니다.")
     @field:Size(max = 3000, message = "내용은 3000자를 넘길 수 없습니다.")
     val codeAndContents: String
 )
